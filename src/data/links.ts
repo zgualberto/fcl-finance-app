5c3056960d090e8878dd5fcc07bfb84be5446673
Ziegfrid Gualberto <ziegfrid.gualberto@gmail.com>
2026-02-12 14:33:43 +0800

    feat: update links list with new structure and icons for improved navigation

--- src/data/links.ts
+++ src/data/links.ts
@@ -2,14 +2,35 @@ import type { EssentialLinkProps } from 'components/EssentialLink.vue';
 
 export const linksList: EssentialLinkProps[] = [
   {
-    title: 'FCL Weekly Collection',
-    icon: 'fa-solid fa-dollar-sign',
+    title: 'Collection',
+    icon: 'fa-solid fa-house',
     routeName: 'weekly_collections',
   },
   {
-    title: 'FCL Weekly Expenses',
-    icon: 'receipt',
+    title: 'Expenses',
+    icon: 'fa-solid fa-file-invoice-dollar',
     routeName: 'expenses',
+  },
+  {
+    title: 'Management',
+    icon: 'fa-solid fa-cogs',
+    children: [
+      {
+        title: 'Members',
+        icon: 'fa-solid fa-users',
+        routeName: 'admin.members',
+      },
+      {
+        title: 'Categories',
+        icon: 'fa-solid fa-tags',
+        routeName: 'admin.categories',
+      },
+      {
+        title: 'Activity Logs',
+        icon: 'fa-solid fa-list',
+        routeName: 'admin.activity-logs',
+      },
+    ],
   },
   {
     title: 'Reports',
--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,80 +1,54 @@
 <template>
   <q-layout view="lHh Lpr lFf">
-    <q-header class="rounded-borders">
-      <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          icon="fa-solid fa-bars"
-          aria-label="Menu"
-          @click="toggleLeftDrawer"
-        />
+    <q-header class="bg-white q-px-xl q-py-md">
+      <q-toolbar class="justify-between">
+        <q-toolbar-title class="text-h5 text-black text-weight-bold"
+          >FCL Finance System</q-toolbar-title
+        >
 
-        <q-toolbar-title> FCL Finance Portal </q-toolbar-title>
-        <q-spacer></q-spacer>
-        <q-btn flat round icon="fa-solid fa-gear" aria-label="Settings">
-          <q-menu>
-            <q-list style="min-width: 250px">
-              <q-item
-                clickable
-                v-close-popup
-                class="row items-center"
-                :to="{ name: 'admin.categories' }"
-              >
-                <q-icon name="fa-solid fa-list"></q-icon>
-                <q-item-section class="text-center text-subtitle1">Categories</q-item-section>
-              </q-item>
-              <q-item
-                clickable
-                v-close-popup
-                class="row items-center"
-                :to="{ name: 'admin.members' }"
-              >
-                <q-icon name="fa-solid fa-user-cog"></q-icon>
-                <q-item-section class="text-center text-subtitle1">Members</q-item-section>
-              </q-item>
-              <!-- activity logs -->
-              <q-item
-                clickable
-                v-close-popup
-                class="row items-center"
-                :to="{ name: 'admin.activity-logs' }"
-              >
-                <q-icon name="fa-solid fa-bug"></q-icon>
-                <q-item-section class="text-center text-subtitle1">Activity Logs</q-item-section>
-              </q-item>
-              <!-- backup -->
-              <!-- <q-item clickable v-close-popup class="row items-center">
-                <q-icon name="fa-solid fa-database"></q-icon>
-                <q-item-section class="text-center text-subtitle1">Backup</q-item-section>
-              </q-item> -->
-            </q-list>
-          </q-menu>
-        </q-btn>
+        <div class="row items-center q-gutter-sm">
+          <q-btn
+            v-for="(link, index) in linksList"
+            :key="index"
+            :to="link.routeName ? { name: link.routeName } : undefined"
+            no-caps
+            rounded
+            :class="{ 'text-black': link.routeName !== $router.currentRoute.value.name }"
+            :flat="link.routeName !== $router.currentRoute.value.name"
+            :color="link.routeName === $router.currentRoute.value.name ? 'primary' : 'dark'"
+            :unelevated="link.routeName === $router.currentRoute.value.name"
+            class="q-py-sm q-px-md text-subtitle1"
+          >
+            <q-icon v-if="link.icon" :name="link.icon" size="sm" class="q-mr-md" />
+            {{ link.title }}
+            <q-menu v-if="link.children && link.children.length > 0">
+              <q-list>
+                <q-item
+                  v-for="child in link.children"
+                  :key="child.routeName"
+                  clickable
+                  :to="{ name: child.routeName }"
+                >
+                  <q-item-section v-if="child.icon" avatar>
+                    <q-icon :name="child.icon" />
+                  </q-item-section>
+                  <q-item-section>
+                    <q-item-label>{{ child.title }}</q-item-label>
+                  </q-item-section>
+                </q-item>
+              </q-list>
+            </q-menu>
+          </q-btn>
+        </div>
       </q-toolbar>
     </q-header>
 
-    <q-drawer v-model="leftDrawerOpen" show-if-above bordered>
-      <q-list class="q-pt-xl">
-        <EssentialLink v-for="link in linksList" :key="link.title" v-bind="link" />
-      </q-list>
-    </q-drawer>
-
-    <q-page-container>
+    <q-page-container class="q-my-lg">
       <router-view />
     </q-page-container>
   </q-layout>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
-import EssentialLink from 'components/EssentialLink.vue';
 import { linksList } from 'src/data/links';
-
-const leftDrawerOpen = ref(false);
-
-function toggleLeftDrawer() {
-  leftDrawerOpen.value = !leftDrawerOpen.value;
-}
 </script>
